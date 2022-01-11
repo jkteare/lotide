c0e5f03f5f5d69acaae68ac6ca8c906359c2a7d0
Jay <jay@teare.ca>
2022-01-10 17:30:29 -0800

    assertEqual function with emoji

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('☑️ Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('❌ Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Blue-Forty-Two", "Blue-Forty-Two");
+assertEqual("Blue-Forty-Two", "Blue-Forty-Three");
+assertEqual(1234, 1234);
+assertEqual(1234, 1235);
