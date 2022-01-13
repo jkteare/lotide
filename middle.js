a633828d9bad8930ab782be6fef177ecfe04d07b
Jay <jay@teare.ca>
2022-01-12 16:41:38 -0800

    linted

--- middle.js
+++ middle.js
@@ -17,42 +17,42 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-const middle = function (inputArr) {
+const middle = function(inputArr) {
 
   // Consider Array that is too short
   if (inputArr.length <= 2) {
-  return [];
+    return [];
   }
     
-   // Consider Array with Odd Number
+  // Consider Array with Odd Number
   if ((inputArr.length % 2) !== 0) {
-  return (inputArr[(inputArr.length - 1) / 2])
+    return (inputArr[(inputArr.length - 1) / 2]);
   }
 
   // Consider Array with even Number
   if ((inputArr.length % 2) === 0) {
-    indexOne = ((inputArr.length /2) -1)
-    return inputArr.slice(indexOne, indexOne +2)
+    let indexOne = ((inputArr.length / 2) - 1);
+    return inputArr.slice(indexOne, indexOne + 2);
   }
-}
+};
 
 
 
 // Test
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
 
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
 
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
 
-assertArraysEqual((middle([1])), []) // => []
-assertArraysEqual((middle([1, 2])), []) // => []
+assertArraysEqual((middle([1])), []); // => []
+assertArraysEqual((middle([1, 2])), []); // => []
 
-assertArraysEqual((middle([1, 2, 3])), [2])
-assertArraysEqual((middle([1, 2, 3, 4, 5])),[3])
+assertArraysEqual((middle([1, 2, 3])), [2]);
+assertArraysEqual((middle([1, 2, 3, 4, 5])),[3]);
 
-assertArraysEqual((middle([1, 2, 3, 4])),[2, 3])
-assertArraysEqual((middle([1, 2, 3, 4, 5, 6])), [3, 4])
+assertArraysEqual((middle([1, 2, 3, 4])),[2, 3]);
+assertArraysEqual((middle([1, 2, 3, 4, 5, 6])), [3, 4]);
