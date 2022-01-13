0828f7cf63a2927382c647bf8e75dba57c60d01d
Jay <jay@teare.ca>
2022-01-12 16:34:33 -0800

    linted

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,23 +6,23 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const findKeyByValue = function (inputObj, value) {
+const findKeyByValue = function(inputObj, value) {
   for (const key in inputObj) {
     if (inputObj[key] === value) {
-      return key
+      return key;
     }
   }
-}
+};
 
 // Test
 
-const bestTVShowsByGenre = { 
+const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
 
-console.log(findKeyByValue(bestTVShowsByGenre, 'The Wire'))
+console.log(findKeyByValue(bestTVShowsByGenre, 'The Wire'));
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
